49a5265ab20c80577691a951f0397af2954e4e52
UltraBirdRepti <ultrabirdrepti@gmail.com>
2016-06-04 10:48:21 +0900

    リファクタリングを行いました。
    ヘッダー部分を統一のヘッダーファイルに書き出しました。

--- app/controllers/mapping.rb
+++ app/controllers/mapping.rb
@@ -4,7 +4,10 @@ Gurongi::App.controllers :mapping do
     FILTER_WORDS = YAML.load_file(Padrino.root("/lib/filter_words.yml"))
     @translate = {}
   end
-
+  ############################
+  # /mapping
+  # 五十音対応表を表示するController
+  # 
   get :index do
     render 'mapping/index'
   end
