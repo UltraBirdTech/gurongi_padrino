5d679c73c40d8a3454a36ac616b086eed316ae23
UltraBirdRepti <ultrabirdrepti@gmail.com>
2016-06-04 12:49:52 +0900

    Rspec 実装中

---
+++ spec/app/helper/gurongi_helper.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'ExtraWordController',  type: :controller do
+  context '例外単語を特定する'do
+    it '例外単語を特定する' do
+      
+      expect('hoge').to eq 'hoge'
+    end
+  end
+end
