f8b7ac9aaf4fa7e72586f62013ee71204778baec
UltraBirdRepti <ultrabirdrepti@gmail.com>
2017-03-30 20:07:25 +0900

    delete self method

--- lib/filter_words.rb
+++ lib/filter_words.rb
@@ -2,10 +2,6 @@ module PadrinoApp
   # yamlファイルから例外単語を読み込む
   class FilterWords 
     FILTER_WORDS = YAML.load_file(Padrino.root("/lib/filter_words.yml"))
-    
-    def self.[](key)
-      FILTER_WORDS[key]
-    end
   end
 end
 
