b1df1115b909a5f365474d1fc66380bf6c6d0a17
UltraBirdTech <ultrabirdrepti@gmail.com>
2017-11-23 10:02:29 +0900

    repaire rubocop

--- lib/gurongi_mapping.rb
+++ lib/gurongi_mapping.rb
@@ -1,11 +1,10 @@
 module PadrinoApp
   # yamlファイルからマッピングデータを読み込む
   class MappingTable
-    Mapping = YAML.load_file(Padrino.root("/lib/mapping_table.yml"))
-    
+    Mapping = YAML.load_file(Padrino.root('/lib/mapping_table.yml'))
+
     def self.[](key)
       Mapping[key]
     end
   end
 end
-
